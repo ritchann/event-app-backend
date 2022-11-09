a8d081ae40a814c929272da165314d6934165d3a
Glebova Anna <35535292+ritchann@users.noreply.github.com>
2022-11-09 18:15:01 +0100

    update

--- lib/index.ts
+++ lib/index.ts
@@ -5,6 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
 
-app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname + "/build/index.html"));
-});
+// app.get("*", (_, res) => {
+//   res.sendFile(path.join( "/build/index.html"));
+// });
