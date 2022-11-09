1bf12d56c7edc87aa832d81941839584293feb31
Glebova Anna <35535292+ritchann@users.noreply.github.com>
2022-11-09 20:29:12 +0100

    update

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -1,24 +1,20 @@
 import { Sequelize } from "sequelize";
 
-export const database = new Sequelize(
-  "postgres://qntqbprdkafzoq:5639eccb4555c8c8d158304a1e09656c53c2d0d590256f80869565b0bfeff1e5@ec2-3-248-121-12.eu-west-1.compute.amazonaws.com:5432/d62jpgju87nnc0",
-  {
-    host: "ec2-3-248-121-12.eu-west-1.compute.amazonaws.com",
-    database: "d62jpgju87nnc0",
-    username: "qntqbprdkafzoq",
-    port: 5432,
-    password:
-      "5639eccb4555c8c8d158304a1e09656c53c2d0d590256f80869565b0bfeff1e5",
-    dialect: "postgres",
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
+export const database = new Sequelize(process.env.DATABASE_URL, {
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  username: process.env.USERNAME,
+  port: 5432,
+  password: process.env.PASSWORD,
+  dialect: "postgres",
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-  }
-);
+  },
+});
 // {
 //   database: "buildingSite", // name of database
 //   dialect: "postgres",
--- lib/index.ts
+++ lib/index.ts
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
 
-console.log(__dirname);
 app.get("*", (_, res) => {
   res.sendFile(path.join(__dirname + "/build/index.html"));
 });
