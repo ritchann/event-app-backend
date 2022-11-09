de34d1cf7162fc1a5ba668b9308946e9d68246c7
Glebova Anna <35535292+ritchann@users.noreply.github.com>
2022-11-09 21:51:22 +0100

    update

--- lib/config/database.ts
+++ lib/config/database.ts
@@ -1,27 +1,17 @@
 import { Sequelize } from "sequelize";
 
-export const database =
-  process.env.STATUS == "prod"
-    ? new Sequelize(process.env.DATABASE_URL, {
-        host: process.env.PROD_HOST,
-        database: process.env.PROD_DATABASE,
-        username: process.env.PROD_USERNAME,
-        port: 5432,
-        password: process.env.PROD_PASSWORD,
-        dialect: "postgres",
-        ssl: true,
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      })
-    : new Sequelize({
-        database: "",
-        dialect: "postgres",
-        storage: ":memory:",
-        username: "",
-        password: "",
-        host: "",
-      });
+export const database = new Sequelize(process.env.DATABASE_URL, {
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  username: process.env.USERNAME,
+  port: 5432,
+  password: process.env.PASSWORD,
+  dialect: "postgres",
+  ssl: true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
