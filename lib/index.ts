e65ec96646fa911945cc4bcf58986a0ac5ad5b95
Glebova Anna <35535292+ritchann@users.noreply.github.com>
2022-11-13 06:49:57 +0100

    update

--- lib/index.ts
+++ lib/index.ts
@@ -2,6 +2,6 @@ import app from "./app";
 const path = require("path");
 
 const PORT = process.env.PORT || 5000;
-
+console.log(__dirname)
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
 
